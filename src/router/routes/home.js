cab13132d86e5f13fa417af05e67aa5e6628ca08
zibuyu <g1628033027@gmail.com>
2022-08-28 23:27:02 +0800

    fix:新增Entertain2 page

--- src/router/routes/home.js
+++ src/router/routes/home.js
@@ -1,8 +1,31 @@
+import RouteView from '@/layouts/RouteView.vue';
+
 export default {
   path: '/',
   name: 'home',
   meta: {
     title: '首页',
   },
-  component: () => import(/* webpackChunkName: "entertain" */ '@/views/Entertain/Index.vue'),
+  component: RouteView,
+
+  children: [
+    {
+      path: '/home',
+      name: 'Entertain',
+      meta: {
+        title: '邀请',
+        type: 'Entertain'
+      },
+      component: () => import(/* webpackChunkName: "entertain" */ '@/views/Entertain/Index.vue'),
+    },
+    {
+      path: '/home1',
+      name: 'entertain2',
+      meta: {
+        title: '邀请2',
+        type: 'entertain2'
+      },
+      component: () => import(/* webpackChunkName: "entertain" */ '@/views/Entertain2/Index.vue'),
+    },
+  ],
 };
